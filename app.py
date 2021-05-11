f1dbf0296ba474f087b7f5d5a8b548ad0c2fa118
hhh20141045h <sub4155@gmail.com>
2021-05-12 05:00:54 +0900

    20210512수업

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+<!DOCTYPE html>
+<html>
+<body>
+<h1>웹앱프로그래밍</h1>
+
+<p><a href="https://www.w3schools.com/">헬로 페이지</a></p>
+<p><a href="https://www.naver.com/">네이버 페이지</a></p>
+
+</body>
+</html>
+
+    '''
+
+
+@app.route('/naver')
+def naver():
+    return '네이버 페이지'
+    
+if __name__ == '__main__':
+    app.run()
+
