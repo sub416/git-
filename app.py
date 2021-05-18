3b7d563cb6aaa98d14567eb44390b7a947c970d9
Jammin <dudehwoals@gmail.com>
2021-05-18 16:46:05 +0900

    k;lk

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask.wrappers import Request
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,11 +17,23 @@ def index():
 </html>
 
     '''
-
-
 @app.route('/naver')
-def naver():
-    return '네이버 페이지' render_template("main-html")
+def naverhtml():
+    return render_template("naver.html")
+
+@app.route('/gonaver', methods=['GET','POST'])
+def gonaver():
+    if Request.method == 'GET': 
+            return "데이터를 받아주는 페이지" 
+        else:
+            # 여기 POST로 들어오는 데이터를 받아보자
+            search = Request.form['fname']
+            print("전달된값:", search)
+            return "당신이 검색한 키워드(POST)<br>{}".format(search)
+
+
+    
+
     
 if __name__ == '__main__':
     app.run()
